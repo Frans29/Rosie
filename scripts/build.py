88ec748503b79252b442d50d303b626b071227b0
Frans29 <60392722+Frans29@users.noreply.github.com>
2025-01-04 19:40:26 +0200

    Added base files

---
+++ scripts/build.py
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Usage: ./scripts/build
+
+# Note that bdist_wheel command is used to create Python wheel distributions,
+# which are a newer and more efficient format for distributing Python packages
+# than the older sdist format you used previously.
+# python3.9 setup.py sdist
